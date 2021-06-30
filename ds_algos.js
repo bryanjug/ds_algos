6b2539138c16f7a494e683407d0f9fb2ecfba4e6
bryanjug <bryandjug@gmail.com>
2021-06-30 00:29:49 -0700

    another algo

--- ds_algos.js
+++ ds_algos.js
@@ -1423,3 +1423,16 @@ function getPINs(observed) {
     )
   )
 }
+
+//returns the sum of a sequence of integers.
+//If begin value is greater than the end, function should returns 0
+// sequenceSum(2,2,2) === 2
+// sequenceSum(2,6,2) === 12 // 2 + 4 + 6
+// sequenceSum(1,5,1) === 15 // 1 + 2 + 3 + 4 + 5
+// sequenceSum(1,5,3) === 5 // 1 + 4
+const sequenceSum = (begin, end, step) => {
+  if (begin > end) {
+    return 0;
+  }
+  return begin + sequenceSum(begin + step, end, step);
+};
