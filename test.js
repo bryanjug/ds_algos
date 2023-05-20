71c9ba750a177fa943fe5588576bf8b35f2b8065
Bryan Jug <bryandjug@gmail.com>
2023-05-19 22:00:52 -0700

    fibonacci

--- test.js
+++ test.js
@@ -1,20 +1,17 @@
-// Multiple Pointers - isSubsequence
-//Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+function fibonacci(n) {
+    let arr = [0, 1];
+    //[0, 1, 1];
+    for (i = 1; i < n; i++) {
+        let last = i - 1;
+        let current = i;
 
-// Examples:
+        let num = last + current;
+        arr.push(num)
+    }
 
-// isSubsequence('hello', 'hello world'); // true
-// isSubsequence('sing', 'sting'); // true
-// isSubsequence('abc', 'abracadabra'); // true
-// isSubsequence('abc', 'acb'); // false (order matters)
-// Your solution MUST have AT LEAST the following complexities:
-
-// Time Complexity - O(N + M)
-// Space Complexity - O(1)
-
-function isSubsequence() {
-    // good luck. Add any arguments you deem necessary.
-    
+    for (i = 1; i < arr.length; i++) {
+        
+    }
 }
 
-isSubsequence()
+fibonacci(7)
